361477dbec742121264f977f534e9fc5b2ed8670
g12906hk <g12906hk@s30439.tsuda.ac.jp>
2014-04-11 11:03:33 +0900

    HelloWorldと表示します

---
+++ src/First/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package First;
+
+/**
+ *
+ * @author g12906hk
+ */
+public class HelloWorld{
+    
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
